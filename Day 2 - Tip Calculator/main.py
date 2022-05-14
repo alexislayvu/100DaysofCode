7f9ea57c282dbc4dcb59910cb93b86aa78e8f795
Alexis Lay Vu <54639928+alexislayvu@users.noreply.github.com>
2022-05-14 12:45:37 -0700

    Added Day 2

---
+++ Day 2 - Tip Calculator/main.py
@@ -0,0 +1,26 @@
+"""
+Day 2 - Tip Calculator
+May 9, 2022
+"""
+
+print("Welcome to the tip calculator.")
+total_bill = input("What was the total bill? ")
+percentage = input("What percentage tip would you like to give? ")
+total_people = input("How many people to split the bill? ")
+
+# Typecast the data types from str --> int
+new_total_bill = float(total_bill)
+new_total_people = float(total_people)
+new_percentage = float(percentage)
+
+# Calculate the tip percentage
+tip_percentage = new_percentage / 100
+
+# Split the bill
+bill_split = new_total_bill / new_total_people
+
+# Calculate payment per person
+tip = bill_split * tip_percentage
+payment = round(bill_split + tip, 2)
+
+print(f"Each person should pay: ${payment}")
