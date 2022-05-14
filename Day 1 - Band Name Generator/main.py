f0d211ecca04ec170f93ba2ccc3788abfab1a6df
Alexis Lay Vu <54639928+alexislayvu@users.noreply.github.com>
2022-05-14 12:41:45 -0700

    Added Day 1

---
+++ Day 1 - Band Name Generator/main.py
@@ -0,0 +1,10 @@
+"""
+Day 1 - Band Name Generator
+May 8, 2022
+"""
+
+print("Welcome to the Band Name Generator")
+user_city = input("What's the name of the city you grew up in?\n")
+user_pet = input("What's your pet's name?\n")
+
+print(f"Your band name could be {user_city} {user_pet}")
