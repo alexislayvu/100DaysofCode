62adffbf1fc683ebd47725ac3eeccedba8aa5654
Alexis Lay Vu <alexislayvu@csu.fullerton.edu>
2022-06-25 21:49:40 -0700

    Added art.py and updated main.py

--- Day 8 - Caesar Cipher/main.py
+++ Day 8 - Caesar Cipher/main.py
@@ -1,42 +1,49 @@
+import art
+
+print(art.logo)
+
+
+def caesar(message, num_shift, cipher_direction):
+    encoded_text = ""
+    decoded_text = ""
+
+    if cipher_direction == "encode":
+        for letter in message:
+            position = alphabet.index(letter)
+            new_position = position + num_shift
+            if position > 25:
+                new_position -= 25
+                new_letter = alphabet[new_position - 1]
+            else:
+                new_letter = alphabet[new_position]
+
+            encoded_text += new_letter
+
+        print(f"The encoded text is {encoded_text}")
+
+    if cipher_direction == "decode":
+        for letter in message:
+            position = alphabet.index(letter)
+            new_position = position - num_shift
+            new_letter = alphabet[new_position]
+            decoded_text += new_letter
+
+        print(f"The decoded text is {decoded_text}")
+
+
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
             'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+should_continue = True
+while should_continue:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
 
+    caesar(message=text, num_shift=shift, cipher_direction=direction)
 
-def encrypt(message, num_shift):
-    encoded_text = ""
-
-    for letter in message:
-        position = alphabet.index(letter)  # will give back an integer where the index of the letter is
-        new_position = position + num_shift
-        if new_position > 25:
-            new_position -= 25
-            new_letter = alphabet[new_position - 1]
-        else:
-            new_letter = alphabet[new_position]
-
-        encoded_text += new_letter
-
-    print(f"The encoded text is {encoded_text}")
-
-
-def decrypt(message, num_shift):
-    decoded_text = ""
-
-    for letter in message:
-        position = alphabet.index(letter)
-        new_position = position - num_shift
-        new_letter = alphabet[new_position]
-        decoded_text += new_letter
-
-    print(f"The decoded text is {decoded_text}")
-
-
-if direction.lower() == "encode":
-    encrypt(message=text, num_shift=shift)
-if direction.lower() == "decode":
-    decrypt(message=text, num_shift=shift)
-
+    cont = input("Type 'yes' if you want to go again. Otherwise, type 'no'\n.").lower()
+    if cont == "yes":
+        should_continue = True
+    if cont == "no":
+        should_continue = False
