64e0f4e212def4b523927e764ccaa9a54efadf61
Alexis Lay Vu <alexislayvu@gmail.com>
2022-08-08 00:41:15 -0700

    Updated main.py

--- Day 16 - OOP Coffee Machine/main.py
+++ Day 16 - OOP Coffee Machine/main.py
@@ -1,4 +1,4 @@
-from menu import Menu, MenuItem
+from menu import Menu
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
@@ -6,20 +6,19 @@ menu = Menu()
 coffee_maker = CoffeeMaker()
 money_machine = MoneyMachine()
 
-user_choice = input("What would you like? (latte/espresso/cappuccino): ").lower()
-
 machine_on = True
 while machine_on:
+    user_choice = input("What would you like? (latte/espresso/cappuccino): ").lower()
+
     if user_choice == "off":
         machine_on = False
     elif user_choice == "report":
         coffee_maker.report()
         money_machine.report()
-    elif user_choice == "latte" or user_choice == "espresso" or user_choice == "cappuccino":
-        if coffee_maker.is_resource_sufficient(user_choice):
-            payment = money_machine.process_coins()
-            money_machine.make_payment(payment)
-        pass
     else:
-        print("That is not a valid option. Try again!")
+        drink = menu.find_drink(user_choice)
+        if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+            coffee_maker.make_coffee(drink)
+
+
 
